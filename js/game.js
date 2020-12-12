b2ea13c53b1fb58abd13d631cd2562a064009e5c
FionaForodes <74291863+FionaForodes@users.noreply.github.com>
2020-12-12 18:41:48 +0800

    Update game.js

--- js/game.js
+++ js/game.js
@@ -156,5 +156,112 @@
       P:        80,
       Q:        81,
       TILDA:    192
-    }
+    },
+    
+    Runner: {
+
+      initialize: function(id, game, cfg) {
+        this.cfg          = Object.extend(game.Defaults || {}, cfg || {});
+        this.fps          = this.cfg.fps || 60;
+        this.interval     = 1000.0 / this.fps;
+        this.canvas       = document.getElementById(id);
+        this.width        = this.cfg.width  || this.canvas.offsetWidth;
+        this.height       = this.cfg.height || this.canvas.offsetHeight;
+        this.front        = this.canvas;
+        this.front.width  = this.width;
+        this.front.height = this.height;
+        this.back         = Game.createCanvas();
+        this.back.width   = this.width;
+        this.back.height  = this.height;
+        this.front2d      = this.front.getContext('2d');
+        this.back2d       = this.back.getContext('2d');
+        this.addEvents();
+        this.resetStats();
+  
+        this.game = Object.construct(game, this, this.cfg);
+      },
+  
+      start: function() {
+        this.lastFrame = Game.timestamp();
+        this.timer     = setInterval(this.loop.bind(this), this.interval);
+      },
+  
+      stop: function() {
+        clearInterval(this.timer);
+      },
+  
+      loop: function() {
+        var start  = Game.timestamp(); this.update((start - this.lastFrame)/1000.0);
+        var middle = Game.timestamp(); this.draw();
+        var end    = Game.timestamp();
+        this.updateStats(middle - start, end - middle);
+        this.lastFrame = start;
+      },
+  
+      update: function(dt) {
+        this.game.update(dt);
+      },
+  
+      draw: function() {
+        this.back2d.clearRect(0, 0, this.width, this.height);
+        this.game.draw(this.back2d);
+        this.drawStats(this.back2d);
+        this.front2d.clearRect(0, 0, this.width, this.height);
+        this.front2d.drawImage(this.back, 0, 0);
+      },
+  
+      resetStats: function() {
+        this.stats = {
+          count:  0,
+          fps:    0,
+          update: 0,
+          draw:   0, 
+          frame:  0  
+        };
+      },
+  
+      updateStats: function(update, draw) {
+        if (this.cfg.stats) {
+          this.stats.update = Math.max(1, update);
+          this.stats.draw   = Math.max(1, draw);
+          this.stats.frame  = this.stats.update + this.stats.draw;
+          this.stats.count  = this.stats.count == this.fps ? 0 : this.stats.count + 1;
+          this.stats.fps    = Math.min(this.fps, 1000 / this.stats.frame);
+        }
+      },
+  
+      drawStats: function(ctx) {
+        if (this.cfg.stats) {
+          ctx.fillText("frame: "  + this.stats.count,         this.width - 100, this.height - 60);
+          ctx.fillText("fps: "    + this.stats.fps,           this.width - 100, this.height - 50);
+          ctx.fillText("update: " + this.stats.update + "ms", this.width - 100, this.height - 40);
+          ctx.fillText("draw: "   + this.stats.draw   + "ms", this.width - 100, this.height - 30);
+        }
+      },
+  
+      addEvents: function() {
+        Game.addEvent(document, 'keydown', this.onkeydown.bind(this));
+        Game.addEvent(document, 'keyup',   this.onkeyup.bind(this));
+      },
+  
+      onkeydown: function(ev) { if (this.game.onkeydown) this.game.onkeydown(ev.keyCode); },
+      onkeyup:   function(ev) { if (this.game.onkeyup)   this.game.onkeyup(ev.keyCode);   },
+  
+      hideCursor: function() { this.canvas.style.cursor = 'none'; },
+      showCursor: function() { this.canvas.style.cursor = 'auto'; },
+  
+      alert: function(msg) {
+        this.stop(); 
+        result = window.alert(msg);
+        this.start();
+        return result;
+      },
+  
+      confirm: function(msg) {
+        this.stop(); 
+        result = window.confirm(msg);
+        this.start();
+        return result;
+      }
+    } 
   } 
